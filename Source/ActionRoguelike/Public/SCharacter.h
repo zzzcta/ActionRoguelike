731352a80c62c5ad7bf285592b5cd4127e523234
zzzcta <zzzctadev@gmail.com>
2025-10-27 13:24:34 +0100

    Add camera and spring arm components to SCharacter
    Introduced USpringArmComponent and UCameraComponent to ASCharacter for camera handling. Components are created and attached in the constructor, and their pointers are exposed as UPROPERTY for editor visibility.

--- Source/ActionRoguelike/Public/SCharacter.h
+++ Source/ActionRoguelike/Public/SCharacter.h
@@ -6,6 +6,9 @@
 #include "GameFramework/Character.h"
 #include "SCharacter.generated.h"
 
+class USpringArmComponent;
+class UCameraComponent;
+
 UCLASS()
 class ACTIONROGUELIKE_API ASCharacter : public ACharacter
 {
@@ -16,6 +19,11 @@ public:
 	ASCharacter();
 
 protected:
+	UPROPERTY(VisibleAnywhere)
+	USpringArmComponent* SpringArmComp;
+	
+	UPROPERTY(VisibleAnywhere)
+	UCameraComponent* CamaraComp;
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
