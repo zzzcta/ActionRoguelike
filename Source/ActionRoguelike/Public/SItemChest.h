67336dee9285c1d222e5688cfeeec117381bb10c
zzzcta <zzzctadev@gmail.com>
2025-10-28 10:51:47 +0100

    Add SItemChest actor and gameplay interface
    Introduces the SGameplayInterface and SItemChest classes, enabling interactive item chests in the game. Updates DefaultEngine.ini with a property redirect for SItemChest, and adds the ItemChestBP asset for use in the editor.

---
+++ Source/ActionRoguelike/Public/SItemChest.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SGameplayInterface.h"
+#include "GameFramework/Actor.h"
+#include "SItemChest.generated.h"
+
+UCLASS()
+class ACTIONROGUELIKE_API ASItemChest : public AActor, public ISGameplayInterface
+{
+	GENERATED_BODY()
+
+	void Interact_Implementation(APawn* InstigatorPawn);
+	
+public:	
+	// Sets default values for this actor's properties
+	ASItemChest();
+
+	UPROPERTY(EditAnywhere)
+	float TargetPitch;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* BaseMesh;
+
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* LidMesh;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
