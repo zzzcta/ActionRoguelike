233ce9a1841e7de5f8b37d24d7423e543c62a583
zzzcta <zzzctadev@gmail.com>
2025-10-30 12:35:12 +0100

    Add SAttributeComponent for health management
    Introduces USAttributeComponent with a Health property and ApplyHealthChange method. This component can be used to manage health attributes for actors in the ActionRoguelike project.

---
+++ Source/ActionRoguelike/Public/SAttributeComponent.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "SAttributeComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class ACTIONROGUELIKE_API USAttributeComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	USAttributeComponent();
+
+protected:
+	// Called when the game starts
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Attributes")
+	float Health;
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "Attributes")
+	bool ApplyHealthChange(float Delta);
+	
+};
