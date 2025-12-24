94c2a644e7c3ff456fb146fb1bb74b11e7c9f3d8
zzzcta <zzzctadev@gmail.com>
2025-12-24 18:57:06 +0100

    Add interaction widget and highlight for interactables
    Introduced BP_Interaction_Widget and updated SInteractionComponent to detect and highlight interactable actors in real time. Added support for displaying a UI widget when an interactable is focused, and refactored interaction logic to use a focused actor. Updated PlayerCharacter and TestLevel assets to support these changes.

--- Source/ActionRoguelike/Private/SInteractionComponent.cpp
+++ Source/ActionRoguelike/Private/SInteractionComponent.cpp
@@ -1,39 +1,49 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "SInteractionComponent.h"
-
 #include "DrawDebugHelpers.h"
 #include "SGameplayInterface.h"
+#include "SWorldUserWidget.h"
 
 static TAutoConsoleVariable<bool> CVarDebugInteraction(TEXT("su.DebugInteraction"), false, TEXT("Enable debug interaction."), ECVF_Cheat);
 
-void USInteractionComponent::PrimaryInteract()
+USInteractionComponent::USInteractionComponent()
 {
-	bool bDebugInteraction = CVarDebugInteraction.GetValueOnGameThread();
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void USInteractionComponent::TickComponent(float DeltaTime, enum ELevelTick TickType,
+                                           FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	
+	FindInteractable();
+}
+
+void USInteractionComponent::FindInteractable()
+{
+	const bool bDebugInteraction = CVarDebugInteraction.GetValueOnGameThread();
 	
 	FCollisionObjectQueryParams ObjectQueryParams;
-	ObjectQueryParams.AddObjectTypesToQuery(ECC_WorldDynamic);
+	ObjectQueryParams.AddObjectTypesToQuery(CollisionChannel);
 	
-	// FHitResult OutHit;
 	TArray<FHitResult> OutHits;
 	
 	FVector EyeLocation;
 	FRotator EyeRotation;
-	
-	AActor* MyOwner = GetOwner();
+
+	const AActor* MyOwner = GetOwner();
 	MyOwner->GetActorEyesViewPoint(EyeLocation, EyeRotation);
 
-	FVector End = EyeLocation + EyeRotation.Vector() * 500;
+	const FVector End = EyeLocation + EyeRotation.Vector() * TraceLenght;
 
-	float Radius = 30.0f;
 	FCollisionShape Sphere;
 	Sphere.SetSphere(Radius);
 	
-	// bool bBlockingHit = GetWorld()->LineTraceSingleByObjectType(OutHit, EyeLocation, End, ObjectQueryParams);
-
-	bool bBlockingHit = GetWorld()->SweepMultiByObjectType(OutHits, EyeLocation, End, FQuat::Identity ,ObjectQueryParams, Sphere);
-	FColor LineColor = bBlockingHit ? FColor::Green : FColor::Red;
+	const bool bBlockingHit = GetWorld()->SweepMultiByObjectType(OutHits, EyeLocation, End, FQuat::Identity,
+	ObjectQueryParams,Sphere);
+	
+	const FColor LineColor = bBlockingHit ? FColor::Green : FColor::Red;
+	
+	FocusedActor = nullptr;
 	
 	for (FHitResult& Hit : OutHits)
 	{
@@ -47,14 +57,51 @@ void USInteractionComponent::PrimaryInteract()
 		{
 			if (HitActor->Implements<USGameplayInterface>())
 			{
-				APawn* Instigator = Cast<APawn>(MyOwner);
-				ISGameplayInterface::Execute_Interact(HitActor, Instigator);
+				FocusedActor = HitActor;
 				break;
 			}
 		}
 	}
+	
+	if (FocusedActor)
+	{
+		if (DefaultWidgetInstance == nullptr && ensure(DefaultWidgetClass))
+		{
+			DefaultWidgetInstance = CreateWidget<USWorldUserWidget>(GetWorld(), DefaultWidgetClass);
+		}
+		
+		if (DefaultWidgetInstance)
+		{
+			DefaultWidgetInstance->AttachedActor = FocusedActor;
+			
+			if (!DefaultWidgetInstance->IsInViewport())
+			{
+				DefaultWidgetInstance->AddToViewport();
+			}
+		}
+	}
+	else
+	{
+		if (DefaultWidgetInstance)
+		{
+			DefaultWidgetInstance->RemoveFromParent();
+		}
+	}
+	
 	if (bDebugInteraction)
 	{
 		DrawDebugLine(GetWorld(), EyeLocation, End, LineColor, false, 2.0f, 0, 5.0f);
 	}
 }
+
+
+void USInteractionComponent::PrimaryInteract()
+{
+	if (FocusedActor)
+	{
+		APawn* Instigator = Cast<APawn>(GetOwner());
+		ISGameplayInterface::Execute_Interact(FocusedActor, Instigator);
+	}
+}
+
+
--- Source/ActionRoguelike/Public/SInteractionComponent.h
+++ Source/ActionRoguelike/Public/SInteractionComponent.h
@@ -7,11 +7,40 @@
 #include "SInteractionComponent.generated.h"
 
 
+class USWorldUserWidget;
+
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class ACTIONROGUELIKE_API USInteractionComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
 public:	
+	USInteractionComponent();
+	
+protected:
+	UPROPERTY()
+	AActor* FocusedActor{};
+	
+	UPROPERTY(EditDefaultsOnly, Category="Interaction")
+	float Radius{30.0f};
+	
+	UPROPERTY(EditDefaultsOnly, Category="Interaction")
+	float TraceLenght{500.0f};
+	
+	UPROPERTY(EditDefaultsOnly, Category="UI")
+	TSubclassOf<USWorldUserWidget> DefaultWidgetClass{nullptr};
+	
+	UPROPERTY()
+	USWorldUserWidget* DefaultWidgetInstance{nullptr};
+	
+	UPROPERTY(EditDefaultsOnly, Category="Interaction")
+	TEnumAsByte<ECollisionChannel> CollisionChannel{ECC_WorldDynamic};
+	
+	UFUNCTION()
+	void FindInteractable();
+	
+public:
+	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 	void PrimaryInteract();
+	
 };
